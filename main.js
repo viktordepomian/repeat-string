f186eac64874a0337d68d62e46edde3906269d80
viktordepomian <viktor.depomian@gmail.com>
2022-07-12 19:05:27 +0200

    Func in main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const repeatString = (message, repetitions) =>{
+    for(let x = 0; x < repetitions; x++){
+        console.log(message);
+    }
+};
+
+//Please enter your message and how many times you want it repeated!
+repeatString('Whattup', 3);
